49ac7f5caa1258e1706d2c72cc10183a3cf58796
Nick Dobos <ninjoala@gmail.com>
2025-05-06 14:42:31 -0400

    telescope fix

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -18,14 +18,24 @@ telescope.setup({
     },
 })
 
+-- Debug function to ensure it works
+local function debug_find_files()
+    local cwd = vim.fn.getcwd()
+    print("Current working directory:", cwd)
+    print("Running find_files...")
+    builtin.find_files({
+        find_command = {
+            "rg",
+            "--files",
+            "--hidden",
+            "--glob=!.git/*"
+        },
+        cwd = cwd,
+    })
+end
+
 -- Keymaps
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>pf', debug_find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
--- Debug function to check if telescope is working
-local function check_telescope()
-    print("Telescope is loaded")
-    print("Builtin functions available:", vim.inspect(builtin))
-end 
