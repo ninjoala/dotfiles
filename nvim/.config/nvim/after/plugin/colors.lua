8bf5baa49265715cdec18bac1f6b9b5292b9bf28
Nick Dobos <ndobos95@gmail.com>
2025-05-28 12:09:53 -0400

    fix some colors

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -1,10 +1,12 @@
-
 function ColorMyPencils(color)
-	color = color or "rose-pine"
+	-- switch to a higher-contrast theme
+	color = color or "tokyonight"
+
 	vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	-- disable transparent background overrides for better contrast
+	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 ColorMyPencils()
