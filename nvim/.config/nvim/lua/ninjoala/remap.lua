c2a6f593ba1a69091fc20e27ccf43f924d10081d
Nick Dobos <ninjoala@gmail.com>
2025-06-03 21:24:21 -0400

    tmux and nvim remaps

--- nvim/.config/nvim/lua/ninjoala/remap.lua
+++ nvim/.config/nvim/lua/ninjoala/remap.lua
@@ -92,12 +92,13 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local bufnr = args.buf
     local opts = { buffer = bufnr, noremap = true, silent = true }
     
-    -- LSP navigation
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+    -- LSP navigation with Telescope
+    vim.keymap.set('n', 'gd', require('telescope.builtin').lsp_definitions, opts)
+    vim.keymap.set('n', 'gD', require('telescope.builtin').lsp_type_definitions, opts)
+    vim.keymap.set('n', 'gi', require('telescope.builtin').lsp_implementations, opts)
+    vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, opts)
+    vim.keymap.set('n', '<leader>ws', require('telescope.builtin').lsp_workspace_symbols, opts)
+    vim.keymap.set('n', '<leader>ds', require('telescope.builtin').lsp_document_symbols, opts)
     
     -- LSP actions
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
@@ -105,13 +106,10 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
     vim.keymap.set('i', '<C-h>', vim.lsp.buf.signature_help, opts)
     
-    -- Diagnostics
-    vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, opts)
+    -- Diagnostics with Telescope
+    vim.keymap.set('n', '<leader>d', require('telescope.builtin').diagnostics, opts)
     vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
     vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-    
-    -- Workspace
-    vim.keymap.set('n', '<leader>ws', vim.lsp.buf.workspace_symbol, opts)
   end,
 })
 
